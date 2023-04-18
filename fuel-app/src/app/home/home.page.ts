5adf991e1638ac7155900f4e03f3c12b2a038c01
milton.neto <milton.neto@parnamirim.rn.gov.br>
2023-04-18 15:59:55 -0300

    fuelApp utilizando ReactformsModules

--- fuel-app/src/app/home/home.page.ts
+++ fuel-app/src/app/home/home.page.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { IonicModule, IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, ReactiveFormsModule],
 })
 export class HomePage {
-  expressao: string;
+  resultado: number;
+  mensagem: string;
+  cor: string;
+  formGroup: FormGroup;
 
-  constructor() {
-    this.expressao = '';
+  constructor(private fb: FormBuilder) {
+    this.resultado = 0;
+    this.mensagem = '';
+    this.cor = '#fff';
+    this.formGroup = fb.group({
+      alcool: ['', [Validators.required]],
+      gasolina: ['', [Validators.required, Validators.min(0.1)]],
+    });
+  }
+  calcular() {
+    const precos = this.formGroup.value;
+    this.resultado = +precos.alcool / +precos.gasolina;
+    if (this.resultado <= 0.7) {
+      this.mensagem = 'Melhor abastecer com álcool!';
+      this.cor = 'success';
+    } else {
+      this.mensagem = 'Melhor abastecer com gasolina!';
+      this.cor = 'tertiary';
+    }
+  }
+  limpar() {
+    this.formGroup.setValue({ alcool: '', gasolina: '' });
+    this.cor = '#fff';
+    this.mensagem = '';
+    this.resultado = 0;
   }
 }
